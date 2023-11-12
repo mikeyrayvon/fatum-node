d7a2f9009b3e8da69725561c878da3f17ee3231c
Michael Ray-Von <michaelrayvon@gmail.com>
2023-11-12 15:50:38 +0100

    update index with exports

--- index.mjs
+++ index.mjs
@@ -1,10 +1,8 @@
-import { getAttractor, getVoid } from './core/geo.mjs'
+import { getAttractor, getVoid } from "./core/geo.mjs";
+import { getRand } from "./core/qrnd.mjs";
 
-const hb = {lat: 41.982937, lon: -87.906416}
-getAttractor(hb, 3000)
-  .catch(e => console.error(e))
-  .then(v => console.log(v))
-
-getVoid(hb, 3000)
-  .catch(e => console.error(e))
-  .then(v => console.log(v))
+module.exports = {
+  getAttractor,
+  getVoid,
+  getRand,
+};
